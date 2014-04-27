be8f32353a2fd2fb4463f643b31cf5fa5a3f6b63
Michael Provencher <michaelprovencher15@gmail.com>
2014-04-27 02:01:41 -0400

    Strucutre de fichiers

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CDCollection::Application.config.secret_key_base = 'f1f86c03361c890ce71bc0bb92eb2aabf3c583d9b3597a6ff95df7a47a18d46a4d15dcc31104b6bf90f7ee4a68db2a6de50fe18c39c4cbe9986cde705149332b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CDCollection::Application.config.session_store :cookie_store, key: '_CDCollection_session'
