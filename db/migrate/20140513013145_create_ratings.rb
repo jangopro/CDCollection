7b29b5de3baaf17a7721c99ec547bdb6e30dc536
Michael Provencher <michaelprovencher15@gmail.com>
2014-05-25 02:15:55 -0400

    Possibilités des noter avec des demies-étoiles

--- db/migrate/20140513013145_create_ratings.rb
+++ db/migrate/20140513013145_create_ratings.rb
@@ -2,7 +2,7 @@ class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
       t.integer :album_id
-      t.decimal :rating
+      t.double precision :rating
       t.integer :user_id
 
       t.timestamps
