6873e260cd4bb64169a1bfbdbc3b63a4747c7164
Michael Provencher <michaelprovencher15@gmail.com>
2016-01-11 18:04:14 +0000

    Rajouter des données pour seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,7 @@ Artiste.create([{nom: 'Rush'}, {nom: 'Linkin Park'}, {nom: 'Tool'}])
 Album.create([{name: 'Moving Pictures', anneeSortie: 1981, artiste_id: 1, typeAlbum: 1}, 
               {name: 'Rush', anneeSortie: 1974, artiste_id: 1, typeAlbum: 1},
               {name: 'Fly By Night', anneeSortie: 1975, artiste_id: 1, typeAlbum: 1},
-              {name: 'Caress of Steel', anneeSortie: 1975, artiste_id: 1, typeAlbum: 1}])
+              {name: 'Caress of Steel', anneeSortie: 1975, artiste_id: 1, typeAlbum: 1},
+              {name: '10,000 Days', anneeSortie: 2006, artiste_id: 2, typeAlbum: 1},
+              {name: 'Lateralus', anneeSortie: 2001, artiste_id: 2, typeAlbum: 1},
+              {name: 'Opiate', anneeSortie: 1992, artiste_id: 2, typeAlbum: 2}])
